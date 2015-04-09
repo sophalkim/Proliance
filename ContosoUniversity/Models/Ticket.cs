4537ff3094c9bc9110a5043f41bf46972b87b0fb
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-08 22:33:59 -0700

    Added tickets mvc

---
+++ ContosoUniversity/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace ContosoUniversity.Models
+{
+    public class Ticket
+    {
+        public int ID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime Date { get; set; }
+        public int amount { get; set; }
+    }
+
+    public class TicketDbContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
